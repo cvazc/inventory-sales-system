8219833bb6d885114d01ac9e029149a8fb8812c9
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2026-01-05 19:58:55 +0000

    Register ProductService in DI container and inject it into minimal API endpoints

--- InventorySalesSystem.Api/Program.cs
+++ InventorySalesSystem.Api/Program.cs
@@ -3,17 +3,17 @@ using InventorySalesSystem.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<ProductService>();
+
 var app = builder.Build();
 
-var productService = new ProductService();
-
-app.MapGet("/products", () =>
+app.MapGet("/products", (ProductService productService) =>
 {
    var products = productService.GetAll();
    return Results.Ok(products);
 });
 
-app.MapPost("/products", (Product product) =>
+app.MapPost("/products", (ProductService productService, Product product) =>
 {
     var createdProduct = productService.Create(product);
     return Results.Created($"/products/{createdProduct.Id}", createdProduct);
