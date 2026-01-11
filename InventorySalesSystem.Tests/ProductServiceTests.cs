bcaff8d24f8315c3f47d20010e522ee2dde910c4
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2026-01-11 23:30:50 +0000

    Add xUnit test project and unit tests for ProductService stock rules

---
+++ InventorySalesSystem.Tests/ProductServiceTests.cs
@@ -0,0 +1,35 @@
+using InventorySalesSystem.Api.Data;
+using InventorySalesSystem.Api.Exceptions;
+using InventorySalesSystem.Api.Models;
+using InventorySalesSystem.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+public class ProductServiceTests
+{
+    [Fact]
+    public async Task AdjustStockAsync_ShouldThrow_WhenStockWouldGoBelowZero()
+    {
+        var options = new DbContextOptionsBuilder<InventoryDbContext>()
+            .UseInMemoryDatabase(databaseName: "ProductServiceTests_AdjustStockBelowZero")
+            .Options;
+
+        using var dbContext = new InventoryDbContext(options);
+
+        dbContext.Products.Add(new Product
+        {
+            Sku = "PROD-TEST",
+            Name = "Test Product",
+            StockQuantity = 2,
+            Price = 10,
+            IsActive = true,
+            CreatedAt = DateTime.UtcNow
+        });
+
+        await dbContext.SaveChangesAsync();
+
+        var service = new ProductService(dbContext);
+
+        await Assert.ThrowsAsync<BadRequestException>(() => service.AdjustStockAsync(productId: 1, delta: -999));
+    }
+}
