d588daf7a1f72547bd6b6dcc7041a6ba9dca9054
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2026-01-20 16:54:17 +0000

    Remove template files and clean up unused code

--- InventorySalesSystem.Application/Events/SaleEventPublisher.cs
+++ InventorySalesSystem.Application/Events/SaleEventPublisher.cs
@@ -3,7 +3,7 @@ namespace InventorySalesSystem.Application.Events;
 public class SaleEventPublisher
 {
     public event EventHandler<SaleCreatedEvent>? SaleCreated;
-    
+
     public void PublishSaleCreated(SaleCreatedEvent payload)
     {
         SaleCreated?.Invoke(this, payload);
